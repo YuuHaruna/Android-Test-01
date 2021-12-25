b68e3e5782a09cb38ed2fff8d4171c15a83e6b5a
Nguyen Hoang Dang <lophocpzo@gmail.com>
2021-12-25 21:39:47 +0700

    Fix bug receive date did not appear in receive fragment

--- app/src/main/java/com/example/androidtest1/RecieveFragment.kt
+++ app/src/main/java/com/example/androidtest1/RecieveFragment.kt
@@ -8,6 +8,7 @@ import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.example.androidtest1.databinding.FragmentRecieveBinding
+import java.util.*
 
 class RecieveFragment : Fragment() {
     private lateinit var binding: FragmentRecieveBinding
@@ -28,6 +29,7 @@ class RecieveFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        val cal = Calendar.getInstance()
         with(binding){
             toolbarReceivefragment.inflateMenu(R.menu.menu_receive)
             textviewReceiveSubject.text = args.subject
@@ -35,6 +37,7 @@ class RecieveFragment : Fragment() {
             textviewReceiveReceiver.text = getString(R.string.text_receive_receiver, args.receiver)
             textviewReceiveBody.text = args.body
             textViewReceiveAvatar.text = args.sender.firstOrNull().toString().uppercase()
+            textviewReceiveDate.text = getString(R.string.text_receive_date, cal.get(Calendar.DATE), cal.get(Calendar.MONTH) + 1, cal.get(Calendar.YEAR))
         }
         binding.toolbarReceivefragment.setNavigationOnClickListener {
             findNavController().navigateUp()
